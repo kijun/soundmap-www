ed4edd410eade828ba6b3a8fbf771c02edb431ab
Kijun Seo <kijunseo@gmail.com>
2020-12-30 12:17:01 +0900

    Update

--- script.js
+++ script.js
@@ -12,6 +12,54 @@ var GAME_URL = "https://archive.org/embed/msdos_Prince_of_Persia_1990";
 
 var AUDIO_URL = "http://newtype2016.org/E/section3_sound_v1.mp3";
 
+class Pin {
+  constructor(x, y, large, onClick) {
+    this.x = x;
+    this.y = y;
+    this.large = large;
+    this.onClick = onClick;
+  }
+
+  distance(other) {
+  }
+
+  render() {
+    var cls = this.large ? "pin large" : "pin";
+    if (!this.el) {
+      this.el = $(`<button class="${cls}"></button>`);
+      $('#map').append(this.el);
+      this.el.css('left', this.x*100 + "%");
+      this.el.css('top', this.y*100 + "%");
+      this.el.click(this.onClick);
+    }
+  }
+}
+
+class Line {
+  constructor(pin1, pin2) {
+    this.pin1 = pin1;
+    this.pin2 = pin2;
+  }
+
+  render() {
+    if (!this.el) {
+      this.el = $(`<div class="line"></div>`);
+    }
+  }
+}
+
+var pins = {
+  "1" : [
+    new Pin(0.3, 0.15, true, function(){ showVideo(1) }),
+    new Pin(0.2, 0.10, false, function(){ alert("asdfasdF")}),
+    new Pin(0.1, 0.10, false, function(){ alert("ddd")}),
+  ]
+}
+
+/*pins['1'].forEach(element => {
+  element.render();
+});*/
+
 //var myModalEl = document.getElementById('modal-intro');
 function createModal(elemId, onClick) {
   var modalElem = $('#modal-'+elemId);
@@ -21,7 +69,6 @@ function createModal(elemId, onClick) {
   //return new bootstrap.Modal(document.getElementById('modal-'+elemId));
   return modalElem;
 }
-
 
 var everything = function() {
   var mIntro = createModal('intro', function(val){
@@ -35,14 +82,19 @@ var everything = function() {
       showVideo(3);
     }
   });
-  mIntro.modal('show');
+  setTimeout(function(){
+    $(".fader").addClass('fadeout');
+    setTimeout(function(){
+      //$(".fader").removeClass("in");
+      mIntro.modal('show');
+    }, 3500);
+  }, 1000);
 /*
 show intro modal
 hide intro modal
 show question 1
 show movie 1
 */
-
 
   var i = null;
   $("body").mousemove(function() {
@@ -60,6 +112,19 @@ show movie 1
       $(".menu").fadeOut(1000);  
   });
   */
+
+  $("#btn-next").click(function () {
+    if (player) {
+      var mIntro = createModal('next', function(val){
+        if (val == "next") {
+        }
+      });
+      mIntro.modal('show');
+  });
+}
+
+function activateMenu () {
+
 }
 
 function toggleMenu(toggle) {
@@ -70,14 +135,24 @@ function toggleMenu(toggle) {
   }
 }
 
+function changeBackground(filename) {
+  if (filename) {
+    $('body').css('background-image', 'url("' + filename + ')');
+  } else {
+    $('body').css({'background-image': 'none'});
+  }
+}
+
+var player;
 function showVideo(index) {
   //var iframe = document.querySelector('iframe');
+  changeBackground();
 
   var options01 = {
     url: "https://vimeo.com/495120707/1c7f5341f7"
   };
 
-  var player = new Vimeo.Player('player', options01);
+  player = new Vimeo.Player('player', options01);
 
   player.on('play', function() {
     console.log('Played the video');
@@ -87,6 +162,7 @@ function showVideo(index) {
   player.on('ended', function() {
     console.log('ended the video');
     player.destroy();
+    player = null;
   });
 
   player.getVideoTitle().then(function(title) {
