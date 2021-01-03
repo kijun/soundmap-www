9cf2b8e2715ba575369317780ef8d1be78566cca
Kijun Seo <kijunseo@gmail.com>
2021-01-03 12:52:04 +0900

    New modal template

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 (function() {
-var VIDEO_APPEND = "&modestbranding=0&showinfo=0&rel=0"
-var VIDEO_URLS = {
-  S1AD: "https://www.youtube.com/embed/qjav2fe3oBY?autoplay=0",
-  S2AD: "https://www.youtube.com/embed/-zRvrvHP-PQ?autoplay=0",
-  S2PERFORMANCE: "https://www.youtube.com/embed/k3Irn7EQMl0?autoplay=1",
-  S3AD: "https://www.youtube.com/embed/R-KUTsj7IWs?autoplay=0",
-  CREDITS: "https://www.youtube.com/embed/HCeM-uQ9qn4?autoplay=0"
-}
+
 var videos = [
   "https://vimeo.com/496053974", // santa - real
   "https://vimeo.com/496030848", // ginari - real
@@ -14,8 +7,17 @@ var videos = [
   "https://vimeo.com/496030173", // budren - virtual 
 ]
 
-var vr = "https://hubs.mozilla.com/tJPQ7Pr/pastel-twin-universe?embed_token=6b20f7f4f25c9bd86da91bcdd14d0610";
-//<iframe src="https://hubs.mozilla.com/tJPQ7Pr/pastel-twin-universe?embed_token=6b20f7f4f25c9bd86da91bcdd14d0610" style="width: 1024px; height: 768px;" allow="microphone; camera; vr; speaker;"></iframe>
+var modal_src = document.getElementById("modal-template").innerHTML;
+var modal_tmpl = Handlebars.compile(modal_src);
+
+var modals = {
+  "1": {
+    "body": "hello world",
+    "buttons": [
+      {"value": 1, "text": "asdfasdf"}
+    ]
+  }
+}
 
 class History {
   constructor() {
@@ -54,54 +56,6 @@ var history = new History();
 var GAME_URL = "https://archive.org/embed/msdos_Prince_of_Persia_1990";
 
 var AUDIO_URL = "http://newtype2016.org/E/section3_sound_v1.mp3";
-
-class Pin {
-  constructor(x, y, large, onClick) {
-    this.x = x;
-    this.y = y;
-    this.large = large;
-    this.onClick = onClick;
-  }
-
-  distance(other) {
-  }
-
-  render() {
-    var cls = this.large ? "pin large" : "pin";
-    if (!this.el) {
-      this.el = $(`<button class="${cls}"></button>`);
-      $('#map').append(this.el);
-      this.el.css('left', this.x*100 + "%");
-      this.el.css('top', this.y*100 + "%");
-      this.el.click(this.onClick);
-    }
-  }
-}
-
-class Line {
-  constructor(pin1, pin2) {
-    this.pin1 = pin1;
-    this.pin2 = pin2;
-  }
-
-  render() {
-    if (!this.el) {
-      this.el = $(`<div class="line"></div>`);
-    }
-  }
-}
-
-var pins = {
-  "1" : [
-    new Pin(0.3, 0.15, true, function(){ showVideo(1) }),
-    new Pin(0.2, 0.10, false, function(){ alert("asdfasdF")}),
-    new Pin(0.1, 0.10, false, function(){ alert("ddd")}),
-  ]
-}
-
-/*pins['1'].forEach(element => {
-  element.render();
-});*/
 
 //var myModalEl = document.getElementById('modal-intro');
 function createModal(elemId, onClick) {
@@ -233,8 +187,28 @@ function showVideo(index) {
   });
 }
 
-everything();
+//everything();
 
+
+//var context = { title: "My New Post", body: "This is my first post!" };
+
+//var myModalEl = document.getElementById('modal-intro');
+function showModalFromTmpl(elemId, onClick) {
+  var data = modals[elemId+""];
+  data["modal-id"] = elemId;
+  var html = modal_tmpl(data);
+  var result = $("body").append(html);
+  console.log(html);
+  var modalElem = $('#modal-'+elemId);
+  console.log(modalElem);
+  modalElem.find('button').click(function(){
+    onClick(this.value);
+  });
+  modalElem.modal('show');
+  return modalElem;
+}
+
+showModalFromTmpl("1", function(v) {alert(v)});
 
 })();
 
