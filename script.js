f597f052f62f042c78fe856bdd0cf5ce19dede94
Kijun Seo <kijunseo@gmail.com>
2021-01-05 08:25:39 +0900

    On Next

--- script.js
+++ script.js
@@ -1,51 +1,77 @@
 (function() {
 
 var videos = [
+  "https://vimeo.com/496030173", // budren - virtual 
   "https://vimeo.com/496053974", // santa - real
   "https://vimeo.com/496030848", // ginari - real
   "https://vimeo.com/496142012", // saeta - virtual
-  "https://vimeo.com/496030173", // budren - virtual 
 ]
 
 var modal_src = document.getElementById("modal-template").innerHTML;
 var modal_tmpl = Handlebars.compile(modal_src);
 
 var modal_content = {
-  "1": {
-    "body": "hello world",
-    "buttons": [
-      {"value": 1, "text": "asdfasdf"}
-    ]
-  },
-  "intro-1": {
+  "p1": {
     "body": `
     <p>
-    <b>다중 공간에서의 사운드맵 프로젝트</b>는 
-    장소와 공간 기반의 공연 형식의 확장 가능성에 대한 실험과
-    동시에 온라인 미디어 기반 예술 창작물을 제작하는 의도로 기획되었습니다.
+    ‘다중 공간에서의 사운드맵 프로젝트’는 장소와 공간 기반의 사운드맵 프로젝트의 공연 형식 확장 가능성에 대한 실험과 동시에 온라인 미디어 기반 예술 창작물을 제작하며 실제와 가상, 아날로그와 디지털 그리고 오프라인과 온라인 플랫폼에서 작업할 때 우리가 근본적으로 다루어야 하는 가치에 대해 탐색합니다. 
     </p>
-
     <p>
-    총 네 개의 다른 공간, 음악, 영상을 20분에 걸쳐 감상하실 수 있습니다.
+    이 프로젝트는 실제 공간 네 곳을 정하고, 그 곳에서 많이 불리던 음악을 선택한 뒤 가창자의 노래를 전자음악으로 편곡하였습니다. 정가, 경기민요, 서도민요, 남도민요(판소리) 가창자는 이수대엽 버들은, 경기민요 산타령, 서도민요 긴아리, 남도민요 새타령을 불렀습니다. 이 중 두 곡은 실제공간에서 실사 촬영을 통해 영상이 제작이 되었고, 두 곡은 가상공간 안에서 제작이 되었습니다. 관객은 서로 다른 공간, 다른 스타일의 음악, 영상을 경험하면서 버추얼 안에서 시적 여행을 하게 됩니다. 
     </p>
-    `,
-    "buttons": [
-      {"text": "시작하기"}
-    ]
+    <p>
+    각기 다른 지역성과 다양한 어법이 섞이는 음악의 형태처럼 관객은 일차원적인 관람이 아닌 던져진 화두 속에서 답을 찾아가는 여정에서 혹은 다중적 공간에 쌓아올린 새로운 층위에서 자신만의 공감각적 음악체험을 하게 됩니다. 순간의 찰나에서부터 대지의 깊은 숨결까지, 미래적이고 추상적인 것에서 부터 따뜻하고 서정적인 것까지, 평면의 웹을 넘어 입체의 가상까지, 실제 공간에서 시작된 음악의 여정은 작가의 심상 그 이상으로 새로운 공간의 축을 확장하게 됩니다.  
+    </p>
+    `
   },
-  "intro-2": {
+  "p2": {
     "body": `
-    "2017년도 사운드맵의 주제는 한강을 따라가는 소리지도였다." <br><br>
-    소리로 그린 지도의 연장선:<br>한강의 서쪽(파주, DMZ 부근)<br>동쪽(두물머리, 동해로 흐르는)<br><br>"서도민요와 강원민요를 두 축으로 나누어 음악을 재단하였다"`,
-    "buttons": [
-      {"text": "다음으로"}
-    ]
+    <p>
+    조은희는 사운드, 전자음악을 기반으로 다양한 영역의 매체 음악을 만들고 공연을 만들어 왔다. 음악과 노이즈, 서양 전통음악과 한국 전통음악, 사운드 스케이프와 전자음악을 유연하게 엮어내는 방식을 지향해 왔으며 연극, 무용, 영상, 미술, 서커스 등 다양한 예술 분야에서 음악을 작곡, 연주해왔다. 
+    </p>
+    <p>
+    2015년부터 현재까지 공간과 장소성에 주목하는 '사운드맵 프로젝트'를 기획, 공연해오고 있으며 2020년을 전후로 온/오프라인의 특성을 비교하며 공연의 형태를 모색하는 중이다. 
+    </p>`
   },
-  "intro-3": {
-    "body": "다중공간에서의 사운드맵 프로젝트 3",
-    "buttons": [
-      {"text": "다음"}
-    ]
+  "p3": {
+    "body": `<img src="image/p3.png">`,
+  },
+  "p4": {
+    "body": "첫 번째 행선지는 버들은(teaser)입니다.",
+  },
+  "p5": {
+    "body": `
+    <i>버들은 실이 되고<br>
+    꾀꼬리는 북이 되어<br>
+    구십삼춘에 짜 내느니 나의 시름</i>`,
+  },
+  "p6": { 
+    "body": "Movie1",
+  },
+  "p7": { 
+    "body": `팬데믹의 시대에 우리의 세계는 오히려 확장되기도 합니다.<br>
+    전자음악과 한국 전통음악의 결합과 같이<br>
+    실제와 가상의 경계에서 짧은 꿈을 꾸기도 하죠.`,
+  },
+  "p8": { 
+    "body": `<img src="image/p8.png">`
+  },
+  "p9": { 
+    "body": "Movie2"
+  },
+  "p10": { 
+    "body": "자, 그럼 이제 어떤 공간으로 이동해볼까요?"
+  },
+  "p11-1": { 
+    "body": `이제 우리의 마지막 행선지입니다.
+     <audio autoplay>  <source src="audio/pado-1.wav" type="audio/wav"> </audio>`
+  },
+  "p11-2": { 
+    "body": `이제 우리의 마지막 행선지입니다.
+     <audio autoplay>  <source src="audio/pado-1.wav" type="audio/wav"> </audio>`
+  },
+  "p12": { 
+    "body": `함께 여행해주셔서 감사합니다. 모두에게 다른 형태로 기억될 ‘다중 공간에서의 사운드맵 프로젝트’였습니다.`
   },
   "q1": {
     "body": `몸을 배척하는 공간에서 장시간 체류해보신 경험이 있으신가요? 모래에 남은 발자국이 바람에 휩쓸려 사라집니다<br><br>
@@ -175,11 +201,13 @@ var everything = function() {
       //$(".fader").removeClass("in");
       //mIntro.modal('show');
       console.log("showing intro modal");
-      showModal("intro-1",
-        nextModal("audio-1",
-        nextModal("intro-2",
-        nextModal("q1",
-        nextModal("info-1-1",
+      showModal("p1",
+        nextModal("p2",
+        nextModal("p3",
+        nextModal("p4",
+        nextModal("p5",
+        nextModal("p6",
+        nextModal("p7",
         function(val) {
           if (val == "real") {
             Math.random > 0.5 ? showVideo(0) : showVideo(1);
@@ -187,7 +215,7 @@ var everything = function() {
           //if (val == "virtual") {
             Math.random > 0.5 ? showVideo(2) : showVideo(3);
           }
-        })))));
+        })))))));
     }, 3000);
   }, 1000);
 /*
@@ -262,7 +290,7 @@ function changeBackground(filename) {
 }
 
 var player;
-function showVideo(index) {
+function showVideo(index, next) {
   //var iframe = document.querySelector('iframe');
   changeBackground();
   history.watch(index);
@@ -282,7 +310,8 @@ function showVideo(index) {
     console.log('ended the video');
     player.destroy();
     player = null;
-    showNextVideo();
+    next();
+    //showNextVideo();
   });
 
   player.getVideoTitle().then(function(title) {
